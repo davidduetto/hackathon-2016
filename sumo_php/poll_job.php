e1f77f1d6707c3e1a7def1f3d59e1d22d2753611
David Su <david@duettoresearch.com>
2016-12-15 20:33:45 -0800

    Sumo chart

---
+++ sumo_php/poll_job.php
@@ -0,0 +1,19 @@
+<?php
+$ch = curl_init();
+
+$jobLink = $_POST["link"];
+
+curl_setopt($ch, CURLOPT_URL, $jobLink);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+curl_setopt($ch, CURLOPT_USERPWD, "suTQMcVt1XZHU0" . ":" . "stYh2uY2oojhV9HThno5UvSZrb5zhF1L1lU4OvMKJIKHth4isy3tOJLQAvx5JOf0");
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+print $result;
+
+?>
